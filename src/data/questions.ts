cddd4dd1ac96b873613a291be57006be4eb79e4e
Urtzi Aresti Santamaria <uaresti@hiberus.com>
2026-06-24 19:46:33 +0200

    131 ok

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1420,4 +1420,363 @@ export const QUESTIONS: Question[] = [
     },
     correct: "b",
   },
+  {
+    id: 121,
+    question:
+      "En referencia a los colimadores de los aceleradores lineales de electrones modernos, es FALSO que:",
+    options: {
+      a: "Existe un colimador primario de forma cónica, situado a la salida del haz.",
+      b: "El colimador multiláminas (MLC) está formado por pares de láminas enfrentadas unas a otras que pueden moverse independientemente.",
+      c: "Los colimadores secundarios permiten variar el tamaño del campo de radiación.",
+      d: "Tanto el colimador primario como el secundario tienen un sistema de giro que permite orientar la posición del campo de tratamiento.",
+    },
+    correct: "d",
+  },
+  {
+    id: 122,
+    question:
+      "En referencia al proceso radioterápico, aquella planificación en la que el usuario define algunos parámetros de los haces y los objetivos clínicos fijados, y el sistema de planificación proporciona la solución más adecuada se denomina:",
+    options: {
+      a: "Planificación sistemática.",
+      b: "Planificación directa.",
+      c: "Planificación inversa.",
+      d: "Planificación computarizada.",
+    },
+    correct: "c",
+  },
+  {
+    id: 123,
+    question:
+      "En el tratamiento de mamas voluminosas, ¿qué accesorio se utiliza para homogeneizar la diferencia anatómica de la región torácica anterior?",
+    options: {
+      a: "Inmovilizador de tórax.",
+      b: "Sistema de inmovilización individual.",
+      c: "Planos inclinados.",
+      d: "Almohadas, sábanas o toallas.",
+    },
+    correct: "c",
+  },
+  {
+    id: 124,
+    question:
+      "El número de desintegraciones que se producen en una muestra por unidad de tiempo se denomina:",
+    options: {
+      a: "Actividad.",
+      b: "Periodo de desintegración.",
+      c: "Periodo de semidesintegración.",
+      d: "Tiempo de vida medio.",
+    },
+    correct: "a",
+  },
+  {
+    id: 125,
+    question:
+      "¿Dónde se localizan principalmente las células plasmáticas anormales en un plasmocitoma?",
+    options: {
+      a: "Solo en los ganglios linfáticos.",
+      b: "Aislados en el hueso o en el tejido blando.",
+      c: "En la epidermis.",
+      d: "Todas las opciones son correctas.",
+    },
+    correct: "b",
+  },
+  {
+    id: 126,
+    question:
+      "¿Qué tratamiento es más eficaz para tratar las enfermedades articulares inflamatorias?",
+    options: {
+      a: "Sinovectomía radiactiva.",
+      b: "Radioinmunoterapia.",
+      c: "Radiocirugía.",
+      d: "Ninguna opción es correcta.",
+    },
+    correct: "a",
+  },
+  {
+    id: 127,
+    question:
+      "En referencia a las curvas de isodosis para fotones y electrones, es cierto que:",
+    options: {
+      a: "Solo se relacionan con el PDD del haz.",
+      b: "Solo se relacionan con el perfil de dosis del haz.",
+      c: "Se pueden relacionar tanto con el PDD como con el perfil de dosis del haz.",
+      d: "No se puede relacionar con ninguno de los parámetros citados.",
+    },
+    correct: "c",
+  },
+  {
+    id: 128,
+    question:
+      "En referencia a los monitores de radiación ambiental en el ámbito hospitalario, señala la afirmación FALSA:",
+    options: {
+      a: "Pueden usarse para estimar las dosis efectivas y equivalentes en los órganos de las personas que trabajan con radiaciones ionizantes.",
+      b: "Suelen ser cámaras de ionización o contadores Geiger-Müller.",
+      c: "Normalmente solo se usan para medir radiación gamma.",
+      d: "Se utilizan para medir los niveles de radiación alrededor de los equipos de radioterapia.",
+    },
+    correct: "c",
+  },
+  {
+    id: 129,
+    question:
+      "En referencia a la interacción de los fotones con la materia, ¿qué ocurre en el proceso de creación de pares electrón-positrón?",
+    options: {
+      a: "El fotón interacciona con el campo electromagnético del núcleo y desaparece como consecuencia de dicha interacción.",
+      b: "El fotón interacciona con el campo electromagnético del núcleo y, tras la interacción, continúa su camino con la energía remanente.",
+      c: "El fotón interacciona con un electrón de la corteza y desaparece como consecuencia de dicha interacción.",
+      d: "El fotón interacciona con un electrón de la corteza y, tras la interacción, continúa su camino con la energía remanente.",
+    },
+    correct: "a",
+  },
+  {
+    id: 130,
+    question:
+      "Aquel detector que permite “contar” o conocer la energía de partículas individuales trabajan en modo:",
+    options: {
+      a: "Pulso.",
+      b: "Integrado.",
+      c: "Individual.",
+      d: "Corriente.",
+    },
+    correct: "a",
+  },
+  {
+    id: 131,
+    question:
+      "Con el fin de conseguir focos térmicos mayores y focos efectivos menores en el ánodo del tubo de rayos X convencional, una de las posibilidades que hay es:",
+    options: {
+      a: "Modificar el material del ánodo.",
+      b: "Aumentar la velocidad de giro en ánodos giratorios.",
+      c: "Modificar la inclinación del ánodo respecto al haz de electrones incidente.",
+      d: "Ninguna de las respuestas es correcta.",
+    },
+    correct: "c",
+  },
+  {
+    id: 132,
+    question: "Los radionúclidos de baja energía se caracterizan por tener:",
+    options: {
+      a: "Energía media < 0,100 MeV.",
+      b: "Energía media < 0,050 MeV.",
+      c: "Energía media entre 0,050 y 0,100 MeV.",
+      d: "Energía media > 0,050 MeV.",
+    },
+    correct: "a",
+  },
+  {
+    id: 133,
+    question:
+      "La etapa del tratamiento radioterápico en la cual se calcula la distribución de dosis absorbida en el volumen blanco y en los distintos órganos se denomina:",
+    options: {
+      a: "Simulación.",
+      b: "Prescripción dosimétrica.",
+      c: "Planificación dosimétrica.",
+      d: "Informe dosimétrico.",
+    },
+    correct: "c",
+  },
+  {
+    id: 134,
+    question:
+      "¿Qué ventaja principal tienen los fijadores dentales como sistema de inmovilización?",
+    options: {
+      a: "No producen efecto bolus.",
+      b: "Son muy económicos.",
+      c: "Son cómodos para el paciente.",
+      d: "No requieren impresión dental previa.",
+    },
+    correct: "a",
+  },
+  {
+    id: 135,
+    question:
+      "En referencia a la clasificación de zonas, las zonas de permanencia limitada:",
+    options: {
+      a: "Son un tipo de zona vigilada, por lo que existe la posibilidad de recibir dosis efectivas superiores a 6 mSv por año oficial.",
+      b: "Son un tipo de zona vigilada, por lo que no existe la posibilidad de recibir dosis efectivas superiores a 6 mSv por año oficial.",
+      c: "Son un tipo de zona controlada, por lo que existe la posibilidad de recibir dosis efectivas superiores a 6 mSv por año oficial.",
+      d: "Son un tipo de zona controlada, por lo que no existe la posibilidad de recibir dosis efectivas superiores a 6 mSv por año oficial.",
+    },
+    correct: "c",
+  },
+  {
+    id: 136,
+    question:
+      'En referencia a los elementos facilitadores de la comunicación, ¿en qué consiste la "congruencia"?',
+    options: {
+      a: "En emplear un discurso técnico estructurado que refuerce la credibilidad profesional.",
+      b: "En reformular el mensaje del interlocutor para confirmar que ha sido comprendido correctamente.",
+      c: "En mantener coherencia entre el lenguaje verbal y no verbal, aunque el mensaje no refleje necesariamente el pensamiento interno.",
+      d: "En la ausencia de contradicción entre el pensamiento, el discurso y la conducta del profesional.",
+    },
+    correct: "d",
+  },
+  {
+    id: 137,
+    question:
+      "¿Cuál de las siguientes características NO es propia de un fotón?",
+    options: {
+      a: "No tiene masa ni carga.",
+      b: "Es de naturaleza ondulatoria.",
+      c: "Su velocidad depende del medio por el que se transmita.",
+      d: "Su energía solo depende de la frecuencia de la onda.",
+    },
+    correct: "c",
+  },
+  {
+    id: 138,
+    question:
+      "La técnica de control de movimiento de órganos internos en la cual se sincroniza la irradiación con la respiración del paciente se denomina:",
+    options: {
+      a: "Tracking.",
+      b: "Gating.",
+      c: "Microcolimador multiláminas.",
+      d: "DDR.",
+    },
+    correct: "b",
+  },
+  {
+    id: 139,
+    question:
+      "Comparándolo con el acelerador lineal de electrones, ¿cuál de las siguientes afirmaciones NO es una ventaja del equipo de cobaltoterapia?",
+    options: {
+      a: "Tiene un índice relativamente bajo de averías.",
+      b: "Produce un menor tamaño de la penumbra del campo de radiación.",
+      c: "El espesor de sus blindajes es menor.",
+      d: "Pueden instalarse en salas de reducido tamaño.",
+    },
+    correct: "b",
+  },
+  {
+    id: 140,
+    question:
+      "El programa de control de calidad de las etapas clínicas de las unidades asistenciales de radioterapia se aplicará:",
+    options: {
+      a: "A todas y cada una de las etapas clínicas del proceso radioterápico.",
+      b: "Solo a las etapas clínicas en las que haya un contacto con el paciente.",
+      c: "Solo a las etapas clínicas en las que haya intercambio de información entre profesionales.",
+      d: "Solo a las etapas clínicas relacionadas con la decisión terapéutica.",
+    },
+    correct: "a",
+  },
+  {
+    id: 141,
+    question:
+      "¿Cuál de los siguientes detectores es el más adecuado para la dosimetría absoluta?",
+    options: {
+      a: "Dosímetro semiconductor.",
+      b: "Cámara de ionización.",
+      c: "Dosímetro por película radiográfica.",
+      d: "Dosímetro por película radiocrómica.",
+    },
+    correct: "b",
+  },
+  {
+    id: 142,
+    question:
+      "En referencia al efecto anódico o efecto talón en el tubo de rayos X convencional, es cierto que:",
+    options: {
+      a: "Consigue la homogeneidad en la intensidad del haz en todo el campo de rayos X.",
+      b: "Produce que la intensidad de la radiación en el lado del cátodo sea mayor que en el lado del ánodo.",
+      c: "Produce que la intensidad de la radiación en el lado del ánodo sea mayor que en el lado del cátodo.",
+      d: "Produce que la intensidad de la radiación en el lado del cátodo sea menor que en el ánodo.",
+    },
+    correct: "b",
+  },
+  {
+    id: 143,
+    question:
+      "En pacientes que utilizan cánula de laringectomía metálica, ¿qué acción se realiza para realizar una TC?",
+    options: {
+      a: "Dejar la cánula metálica.",
+      b: "Cubrir la cánula con bolus.",
+      c: "Retirar la cánula y no poner nada durante la sesión.",
+      d: "Sustituirla por una cánula de plástico.",
+    },
+    correct: "d",
+  },
+  {
+    id: 144,
+    question:
+      "En la braquiterapia de la mucosa yugal, ¿cuál NO es un efecto secundario común?",
+    options: {
+      a: "Odinofagia.",
+      b: "Úlceras.",
+      c: "Mucositis.",
+      d: "Dermitis.",
+    },
+    correct: "d",
+  },
+  {
+    id: 145,
+    question:
+      "Entre los parámetros utilizados para calcular el índice TPR 20/10, NO se encuentra:",
+    options: {
+      a: "Dosis absorbida a profundidad de 10 cm.",
+      b: "Dosis absorbida a profundidad de 20 cm.",
+      c: "Distancia foco superficie (SSD) fija de 100 cm.",
+      d: "Tamaño de campo de 10 cm x 10 cm.",
+    },
+    correct: "c",
+  },
+  {
+    id: 146,
+    question:
+      "En relación a las precauciones de aislamiento para el control de la infección nosocomial, es cierto que:",
+    options: {
+      a: "Deben aplicarse solo cuando la persona paciente tiene un diagnóstico confirmado de una enfermedad altamente contagiosa.",
+      b: "Son necesarias únicamente cuando el personal sanitario entra en contacto con pacientes que presenten síntomas evidentes.",
+      c: "Se deben aplicar en el cuidado de todas las personas pacientes hospitalizadas, sin importar su diagnóstico o si se sospecha que tienen una infección.",
+      d: "Ninguna opción es correcta.",
+    },
+    correct: "c",
+  },
+  {
+    id: 147,
+    question:
+      "El proceso responsable de los efectos biológicos que se producen en la irradiación con rayos X es:",
+    options: {
+      a: "Las ionizaciones que produce la radiación en los átomos del medio.",
+      b: "Las excitaciones que produce la radiación en los átomos del medio.",
+      c: "La atenuación del haz por los átomos del medio.",
+      d: "Todas las respuestas son correctas.",
+    },
+    correct: "a",
+  },
+  {
+    id: 148,
+    question:
+      "¿En qué casos se recomienda utilizar aplicadores de un solo canal en braquiterapia de endometrio?",
+    options: {
+      a: "En tumores ya operados.",
+      b: "En úteros grandes.",
+      c: "En úteros muy pequeños.",
+      d: "Cuando se combina con radioterapia.",
+    },
+    correct: "c",
+  },
+  {
+    id: 149,
+    question:
+      "Entre los equipos que permiten realizar tratamientos con haces de protones encontramos:",
+    options: {
+      a: "El ciclotrón.",
+      b: "El Ciberknife.",
+      c: "Equipos de tomoterapia helicoidal.",
+      d: "Las respuestas a) y c) son correctas.",
+    },
+    correct: "a",
+  },
+  {
+    id: 150,
+    question:
+      "En referencia a la señalización de zonas con riesgo de exposición a las radiaciones ionizantes, una zona en la cual exista solamente riesgo de irradiación externa aparecerá señalizada mediante:",
+    options: {
+      a: "Un trébol enmarcado por una orla rectangular del mismo color que el símbolo.",
+      b: "Un trébol bordeado de puntas radiales enmarcado por una orla rectangular del mismo color que el símbolo.",
+      c: "Un trébol sobre un campo punteado enmarcado por una orla rectangular del mismo color que el símbolo.",
+      d: "Las respuestas b) y c) son correctas.",
+    },
+    correct: "a",
+  },
 ];
