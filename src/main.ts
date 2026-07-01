443c332901eebc4b5e91b68955444e47a43ff20d
Urtzi Aresti Santamaria <uaresti@hiberus.com>
2026-07-01 16:46:26 +0200

    PWA

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+defineCustomElements(window);
+
+
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -14,6 +20,10 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     importProvidersFrom(IonicModule.forRoot()),
     provideHttpClient(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
+          
   ],
 });
