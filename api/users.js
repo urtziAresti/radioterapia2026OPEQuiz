af25bc8b56b89c9cf1112ccafeadc4af4b14ab8e
Urtzi Aresti Santamaria <uaresti@hiberus.com>
2026-07-15 08:48:39 +0200

    deploy 2

--- api/users.js
+++ api/users.js
@@ -1,4 +1,4 @@
-import clientPromise from "../src/app/services/mongodb";
+import clientPromise from "../lib/mongodb.js";
 import bcrypt from "bcryptjs";
 
 export default async function handler(req, res) {
